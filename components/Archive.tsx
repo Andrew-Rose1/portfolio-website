6a58c70e03a1c9d2e0e511a40f119ef677f1b931
Andrew-rose1 <andrew.rose115@gmail.com>
2023-11-16 10:22:05 -0800

    remove "view the archive"

--- components/Archive.tsx
+++ components/Archive.tsx
@@ -8,11 +8,11 @@ const Archive = () => {
     <div className="max-w-contentContainer mx-auto px-4 py-24">
       <div className="w-full flex flex-col items-center">
         <h2 className="text-3xl font-titleFont font-semibold">
-          Other Noteworth Projects
+          Other Noteworthy Projects
         </h2>
-        <p className="text-sm font-titleFont text-textGreen">
+        {/* <p className="text-sm font-titleFont text-textGreen">
           view the archive
-        </p>
+        </p> */}
       </div>
       <div className="grid gir-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 lgl:px-10">
         <ArchiveCard
